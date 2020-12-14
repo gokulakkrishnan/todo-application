5bdcd46c5ba65d788e92680701958764549da876
gokulakkrishnan <gokulvann@gmail.com>
2020-12-14 13:47:40 +0530

    sw

--- routes/routes.js
+++ routes/routes.js
@@ -2,42 +2,50 @@ const handle = require('../handler/handler.js')
 const check = {
     method: 'GET',
     path: '/',
-    handler:handle.checkhost
+    handler:handle.checkhost,
+    tags: ['api']
 };
 const status = {
     method: 'GET',
     path: '/status',
-    handler:handle.checkstatus
+    handler:handle.checkstatus,
+    tags: ['api']
 };
 const register = {
     method: 'POST',
     path: '/api/todo/signUp',
-    handler: handle.signUpNewUser
+    handler: handle.signUpNewUser,
+    tags: ['api']
 };
 const login = {
     method: 'POST',
     path: '/api/todo/login',
-    handler: handle.signInUser
+    handler: handle.signInUser,
+    tags: ['api']
 };
 const getUserById = {
     method: 'GET',
     path: '/api/todo',
-    handler: handle.getUserById
+    handler: handle.getUserById,
+    tags: ['api']
 };
 const createNewUser = {
     method: 'POST',
     path: '/api/todo',
-    handler: handle.createUserTask
+    handler: handle.createUserTask,
+    tags: ['api']
 };
 const updateUserItem = {
     method: 'PUT',
     path: '/api/todo',
-    handler: handle.updateUserItem
+    handler: handle.updateUserItem,
+    tags: ['api']
 };
 const deletelistbyid = {
     method: 'DELETE',
     path: '/api/todo',
-    handler: handle.deleteUserTaskById
+    handler: handle.deleteUserTaskById,
+    tags: ['api']
 };
 module.exports = {
     updateUserItem,
