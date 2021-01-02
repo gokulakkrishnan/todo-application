e56fa201785545bfb4fdc704b2b261bf17ce1d7f
gokulakkrishnan <gokulvann@gmail.com>
2021-01-02 18:07:58 +0530

    as

--- authetication/joivalidate.js
+++ authetication/joivalidate.js
@@ -8,10 +8,6 @@ const loginSchema = joi.object({
     emailId : joi.string().lowercase().email().required(),
     password : joi.string().min(2).required()
 });
-const taskSchema = joi.object({
-    taskName : joi.string().lowercase().required(),
-    taskStatus : joi.string().max(15).required()
-});
 const updateSchema = joi.object({
     taskId : joi.string().uuid().required(),
     taskName : joi.string().max(20),
@@ -19,6 +15,10 @@ const updateSchema = joi.object({
 });
 const deleteSchema = joi.object({
     taskId : joi.string().uuid().required()
+});
+const taskSchema = joi.object({
+    taskName : joi.string().lowercase().required(),
+    taskStatus : joi.string().max(15).required()
 });
 module.exports ={
     signUpSchema,loginSchema,taskSchema,updateSchema,deleteSchema
