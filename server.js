50e401c58202c491d631b263a282f8284d915ab9
gokulakkrishnan <gokulvann@gmail.com>
2021-01-03 12:22:32 +0530

    sd

--- server.js
+++ server.js
@@ -31,43 +31,25 @@ const swaggerOptions = {
             return false
         }
     }
- Promise.allSettled([ server.register([
-    Inert,
-    Vision,
-    {
-        plugin: HapiSwagger,
-        options: swaggerOptions
-    }
-]),
- server.register({
-    plugin: require('hapi-cors'),
-    options: {
-        checkOrigin: checkOrigin
-    }
-}),
- server.start()]).then(()=> console.log("server looking at " + server.info.uri))
-    
-    
-
-// const init = async()=>{
-//     await server.register([
-//         Inert,
-//         Vision,
-//         {
-//             plugin: HapiSwagger,
-//             options: swaggerOptions
-//         }
-//     ]);
-//     await server.register({
-//         plugin: require('hapi-cors'),
-//         options: {
-//             checkOrigin: checkOrigin
-//         }
-//     })
-//    await server.start()
-//     console.log("server looking at " + server.info.uri); 
-// } 
-//init();
+const init = async()=>{
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+    await server.register({
+        plugin: require('hapi-cors'),
+        options: {
+            checkOrigin: checkOrigin
+        }
+    })
+   await server.start()
+    console.log("server looking at " + server.info.uri); 
+} 
+init();
 server.route(router.check)
 server.route(router.status)
 server.route(router.register)
